8eb479b0ef4eb1de01e9e16a77ad1069f79a7af7
Rob Hackett <108783314+Robhack623@users.noreply.github.com>
2022-12-09 11:13:58 -0500

    adding associations

--- server/schemas/planbook.js
+++ server/schemas/planbook.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const planbookSchema = new mongoose.Schema({
     planbook_name: String,
     year: String,
-    schedule_type: String
-
+    schedule_type: String,
+    classes: [{ type: Schema.Types.ObjectId, ref: 'Class'}]
 })
 
 const Planbook = mongoose.model('Planbook', planbookSchema)
--- server/schemas/user.js
+++ server/schemas/user.js
@@ -1,14 +1,34 @@
 
 const mongoose = require('mongoose')
+const PlanbookSchema = require('./planbook')
 
 const userSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    username: String,
+    first_name: {
+        type: String,
+        validate: {
+            validator: first_name => first_name.length > 2,
+            message: 'Name must be longer than 2 characters'
+        }
+    },
+    last_name: {
+        type: String,
+        validate: {
+            validator: last_name => last_name.length > 2,
+            message: 'Name must be longer than 2 characters'
+        }
+    },
+    username: {
+        type: String,
+        validate: {
+            validator: username => username.length > 5,
+            message: 'Name must be longer than 5 characters'
+        }
+    },
     email: String,
     password: String,
     grade_level: String,
-    user_subject: String
+    user_subject: String,
+    planbooks: [PlanbookSchema]
 })
  
 const User = mongoose.model('User', userSchema)
